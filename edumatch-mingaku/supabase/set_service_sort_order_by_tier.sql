b23cd5a299acbce8999422753b8365b019db13d3
Edu-match <edumatch.kanri@gmail.com>
2026-03-06 23:14:22 +0900

    修正

--- edumatch-mingaku/supabase/set_service_sort_order_by_tier.sql
+++ edumatch-mingaku/supabase/set_service_sort_order_by_tier.sql
@@ -2,17 +2,17 @@
 -- プレミアム / スタンダード / ベーシック / その他 のラベルで設定
 
 -- 1. いったんすべて「その他」に
-UPDATE "Service" SET sort_order = 'その他'::"ServiceDisplayTier";
+UPDATE "Service" SET sort_order = 'その他';
 
 -- 2. プレミアム: 合同会社CROP ワンリード
-UPDATE "Service" SET sort_order = 'プレミアム'::"ServiceDisplayTier"
+UPDATE "Service" SET sort_order = 'プレミアム'
 WHERE title ILIKE '%ワンリード%'
    OR "provider_display_name" ILIKE '%ワンリード%'
    OR "provider_display_name" ILIKE '%CROP%'
    OR provider_id IN (SELECT id FROM "Profile" WHERE name ILIKE '%CROP%' OR name ILIKE '%ワンリード%');
 
 -- 3. スタンダード: 青山英語学院 システムASSIST, 英俊社 KAWASEMI Lite, V-Growth, SRJ TERRACE
-UPDATE "Service" SET sort_order = 'スタンダード'::"ServiceDisplayTier"
+UPDATE "Service" SET sort_order = 'スタンダード'
 WHERE title ILIKE '%システムASSIST%' OR title ILIKE '%アシスト%'
    OR title ILIKE '%KAWASEMI%' OR title ILIKE '%カワセミ%'
    OR title ILIKE '%V-Growth%'
@@ -24,7 +24,7 @@ WHERE title ILIKE '%システムASSIST%' OR title ILIKE '%アシスト%'
    OR provider_id IN (SELECT id FROM "Profile" WHERE name ILIKE '%青山英語学院%' OR name ILIKE '%英俊社%' OR name ILIKE '%V-Growth%' OR name ILIKE '%SRJ%');
 
 -- 4. ベーシック: メイツ aim@, okke Dr.okke, ユナイトプロジェクト 塾シル
-UPDATE "Service" SET sort_order = 'ベーシック'::"ServiceDisplayTier"
+UPDATE "Service" SET sort_order = 'ベーシック'
 WHERE title ILIKE '%aim@%' OR title ILIKE '%エイムアット%'
    OR title ILIKE '%Dr.okke%' OR title ILIKE '%okke%'
    OR title ILIKE '%塾シル%'
